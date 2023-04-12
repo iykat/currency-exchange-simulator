28b5f8d2c852c977a5e20782631177ef0b710979
prince takyi <takyip28@gmail.com>
2023-04-12 03:06:16 +0000

    Print list of options

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  // 1 print help
+  std::cout << "1: print help " << std::endl;
+  // 2 print exchange stats
+  std::cout << "2: print exchange stats " << std::endl;
+  // 3 make an offer
+  std::cout << "3: make an offer " << std::endl;
+  // 4 make a bid
+  std::cout << "4: make a bid " << std::endl;
+  // 5 print wallet
+  std::cout << "5: print wallet " << std::endl;
+  // 6 continue
+  std::cout << "6: continue " << std::endl;
+  return 0;
+}

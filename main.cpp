5684453bd801a2c58c298f4e556f2580400b2812
prince takyi <takyip28@gmail.com>
2023-04-14 07:33:44 +0000

    Conditionally print user input

--- main.cpp
+++ main.cpp
@@ -26,5 +26,34 @@ int main()
   std::cin >> userOption;
   std::cout << "you chose: " << userOption << std::endl;
 
+  if (userOption == 0) // bad input
+  {
+    std::cout << "Invalid choice. choose 1-6" << std::endl;
+  }
+  if (userOption == 1) // bad input
+  {
+    std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers" << std::endl;
+  }
+  if (userOption == 2) // bad input
+  {
+    std::cout << "Market looks good" << std::endl;
+  }
+  if (userOption == 3) // bad input
+  {
+    std::cout << "Make an offer - enter the amount" << std::endl;
+  }
+  if (userOption == 4) // bad input
+  {
+    std::cout << "Make a bid - enter the amount" << std::endl;
+  }
+  if (userOption == 5) // bad input
+  {
+    std::cout << "Your wallet is empty" << std::endl;
+  }
+  if (userOption == 6) // bad input
+  {
+    std::cout << "Going to next time frame" << std::endl;
+  }
+
   return 0;
 }

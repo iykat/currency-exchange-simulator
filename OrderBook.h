5eb95fc9ef7202ddf75069b0a636d199764a1780
prince takyi <takyip28@gmail.com>
2023-05-04 07:50:56 +0000

    Set up the OrderBook class

---
+++ OrderBook.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "OrderBookEntry.h"
+#include "CSVReader.h"
+#include <string>
+#include <vector>
+class OrderBook
+{
+public:
+  /** connstruct, reading a csv data file */
+  OrderBook(std::string filename);
+  /** return vector of all known products in the dataset */
+  std::vector<std::string> getKnowonProducts();
+  /** return vector of Orders according to the sent filters */
+  std::vector<OrderBookEntry> getOrders(OrderBookType type,
+                                        std::string product,
+                                        std::string timestamp);
+};

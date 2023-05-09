c1dabd8f13167bc61636dc4fe48fab9a714dd4a5
prince takyi <takyip28@gmail.com>
2023-05-09 17:04:45 +0000

    Add wallet class header file

---
+++ Wallet.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <map>
+
+class Wallet
+{
+public:
+  Wallet();
+  /** insert currency to the wallet */
+  void insertCurrency(std::string type, double amount);
+  /** check if the wallet contains this much currency or more */
+  bool containsCurrency(std::string type, double amount);
+  /** generate a string representation of the wallet */
+  std::string toString();
+
+private:
+  std::map<std::string, double> currencies;
+};

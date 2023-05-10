bd598d54bf142ff112519ad297e37def7ea5c562
prince takyi <takyip28@gmail.com>
2023-05-10 09:40:49 +0000

    Implement insert and contains functions

--- Wallet.cpp
+++ Wallet.cpp
@@ -2,17 +2,32 @@
 
 Wallet::Wallet()
 {
-
 }
 
-void Wallet::insertCurrency(std::string type, double amount) 
+void Wallet::insertCurrency(std::string type, double amount)
 {
-
+  double balance;
+  if (amount < 0)
+  {
+    throw std::exception{};
+  }
+  if (currencies.count(type) == 0) // not there yet
+  {
+    balance = 0;
+  } 
+  else {
+    balance = currencies[type];
+  }
+  balance += amount;
+  currencies[type] = balance;
 }
 
-bool Wallet::containsCurrency(std::string type, double amount) 
+bool Wallet::containsCurrency(std::string type, double amount)
 {
-  return false;
+  if (currencies.count(type) == 0) // not there yet
+    return false;
+  else 
+    return currencies[type] >= amount;
 }
 
 std::string Wallet::toString()
--- main.cpp
+++ main.cpp
@@ -4,12 +4,18 @@
 #include "OrderBookEntry.h"
 #include "MerkelMain.h"
 #include "CSVReader.h"
+#include "Wallet.h"
 
 using namespace std;
 
 int main()
 {
-  MerkelMain app{};
-  app.init();
+  // MerkelMain app{};
+  // app.init();
   // CSVReader::readCSV("data.csv");
+  Wallet wallet;
+  wallet.insertCurrency("BTC", 10);
+  std::cout << "Wallet has btc " << wallet.containsCurrency("BTC", 10) << std::endl;
+
+
 }
